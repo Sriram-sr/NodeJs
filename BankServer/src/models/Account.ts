534848bcf3fbbac872123bdcfb21164604d98e88
Sriram-sr <srirampanneer90@gmail.com>
2024-03-03 19:03:34 +0530

    added required models

---
+++ BankServer/src/models/Account.ts
@@ -0,0 +1,49 @@
+import { Document, Schema, model } from 'mongoose';
+import { UserDocument } from './User';
+import { TransactionDocument } from './Transaction';
+
+export interface AccountDocument extends Document {
+  user: UserDocument;
+  type: 'savings' | 'current';
+  balance: number;
+  dateOpened: Date;
+  dateClosed: Date;
+  status: 'active' | 'inactive';
+  transactions: Array<TransactionDocument>;
+}
+
+const accountSchema = new Schema<AccountDocument>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    type: {
+      type: String,
+      enum: ['savings', 'current'],
+      required: true
+    },
+    balance: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    dateOpened: {
+      type: Date,
+      required: true
+    },
+    dateClosed: Date,
+    transactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Transaction'
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default model<AccountDocument>('Account', accountSchema);
