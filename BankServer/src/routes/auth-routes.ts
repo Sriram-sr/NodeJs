d41f4b55c0fa3787122da0dad2aa2ad26e4d23fe
Sriram-sr <srirampanneer90@gmail.com>
2024-03-07 10:54:54 +0530

    dropping bank server project

--- BankServer/src/routes/auth-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { signupUser } from '../controllers/auth-controllers';
-
-const router = Router();
-
-router.route('/signup').get(signupUser);
-
-export default router;
