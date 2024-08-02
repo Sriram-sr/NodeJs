eda6c13d08b94b16f8152a42125fb7e5799b17d5
Sriram-sr <srirampanneer90@gmail.com>
2024-08-02 18:17:50 +0530

    Jira - minor changes to mongo uri

--- JiraClone/src/utils/constants.ts
+++ JiraClone/src/utils/constants.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 
 config();
 
-const MONGODB_URI = process.env.MOGODB_URI || 'mongodb://localhost:2017/jira';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jira';
 const PORT = process.env.PORT || 8080;
 const JWT_SECURE_KEY = process.env.JWTSECUREKEY || 'SuperComplexToken';
 const JWT_EXPIRY_TIME = process.env.JWTEXPIRY || '10h';
