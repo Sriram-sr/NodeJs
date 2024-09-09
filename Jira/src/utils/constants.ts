6b61219c40291ada56b915ab5d0258cbe25e5c4f
Sriram-sr <srirampanneer90@gmail.com>
2024-09-09 15:25:32 +0530

    Don't want to loose what is got already

--- Jira/src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-
-config();
-
-export const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jira';
-export const PORT = process.env.PORT || 8080;
-export const JWTSECUREKEY = process.env.JWTSECUREKEY || 'SuperComplexToken';
-export const JWTEXPIRY = process.env.JWTEXPIRY || '15m';
